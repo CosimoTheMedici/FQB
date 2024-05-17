fec4be6fa8493662a19e54417ed34467e05ce516
cosmas <cosmasthuku4@gmail.com>
2024-05-17 23:51:34 +0300

    feat:layouts,imports select fields

---
+++ src/pages/modals/UpdateProperties.jsx
@@ -0,0 +1,123 @@
+
+import React, { useState } from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+
+const UpdateProperties = () => {
+  return (
+    <form onSubmit={handleSubmit}>
+            <Form as={Row} >
+              <Col sm="12" lg="6">
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="name">Name:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter name"
+                    onChange={handleChange}
+                    value={propertiesDetails.name}
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm="12" lg="6">
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="address">Address:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="address"
+                    name="address"
+                    onChange={handleChange}
+                    value={propertiesDetails.address}
+                    placeholder="Enter address"
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm="12" lg="6">
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="email">Email address:</Form.Label>
+                  <Form.Control
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={handleChange}
+                    value={propertiesDetails.email}
+                    placeholder="Enter email"
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm="12" lg="6">
+                <Row>
+                  <Col sm="2" lg="2">
+                    <Form.Group className="mb-3">
+                      <Form.Label htmlFor="mobile">Code:</Form.Label>
+                      <Form.Control
+                        type="number"
+                        id="mobile"
+                        name="mobile"
+                        //onChange={handleChange}
+                        value={254}
+                        placeholder="Enter phone number"
+                        disabled
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col sm="10" lg="10">
+                    <Form.Group className="mb-3">
+                      <Form.Label htmlFor="mobile">Phone:712345678</Form.Label>
+                      <Form.Control
+                        type="number"
+                        id="phone"
+                        name="phone"
+                        onChange={handleChange}
+                        value={propertiesDetails.phone}
+                        placeholder="712345678"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Col>
+              <Col sm="12" lg="6">
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="location">Location:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="location"
+                    name="location"
+                    onChange={handleChange}
+                    value={propertiesDetails.location}
+                    placeholder="Enter location"
+                  />
+                </Form.Group>
+              </Col>
+              <Col sm="12" lg="6">
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="description">Description:</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    id="description"
+                    name="description"
+                    onChange={handleChange}
+                    value={propertiesDetails.description}
+                    rows={3}
+                    placeholder="Enter description"
+                  />
+                </Form.Group>
+              </Col>
+              <Row>
+                <Col sm="6" lg="6">
+                  <Button type="submit" variant="btn btn-primary">
+                    Submit
+                  </Button>
+                </Col>
+                <Col sm="6" lg="6">
+                  <Button type="button" variant="btn btn-danger">
+                    cancel
+                  </Button>
+                </Col>
+              </Row>{" "}
+            </Form>
+            </form>
+  )
+}
+
+export default UpdateProperties
---
+++ src/pages/modals/UpdateTenants.jsx
@@ -0,0 +1,165 @@
+
+import React, { useState } from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+
+
+const UpdateTenants = () => {
+  return (
+    <form onSubmit={handleSubmit}>
+    <Form as={Row}>
+     
+
+    <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">Property:</Form.Label>
+
+          
+          <Select
+           arrayData={arrd}
+           defaultSelectText="select "
+           onChange={handleChange}
+           
+           name="select"
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">Unit Name:</Form.Label>
+          <Select
+           arrayData={arrd}
+           defaultSelectText="select "
+           onChange={handleChange}
+           
+           name="select"
+          />
+        </Form.Group>
+      </Col>
+     
+     
+      
+
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">First Name:</Form.Label>
+          <Form.Control
+            type="text"
+            id="first_name"
+            name="first_name"
+            placeholder="Enter First Name"
+            onChange={handleChange}
+            value={tenantDetails.first_name}
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">Last Name:</Form.Label>
+          <Form.Control
+            type="text"
+            id="last_name"
+            name="last_name"
+            placeholder="Enter Last Name"
+            onChange={handleChange}
+            value={tenantDetails.last_name}
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="address">Id Number:</Form.Label>
+          <Form.Control
+            type="number"
+            id="id_number"
+            name="id_number"
+            onChange={handleChange}
+            value={tenantDetails.rent}
+            placeholder="Enter Id Number"
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="email">Email Address:</Form.Label>
+          <Form.Control
+            type="text"
+            id="email_address"
+            name="email_address"
+            onChange={handleChange}
+            value={tenantDetails.email_address}
+            placeholder="Enter Email Address"
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="email">Phone Number:</Form.Label>
+          <Form.Control
+            type="number"
+            id="phone_number"
+            name="phone_number"
+            onChange={handleChange}
+            value={tenantDetails.phone_number}
+            placeholder="Enter Phone Number"
+          />
+        </Form.Group>
+      </Col>
+
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">Emergency Names:</Form.Label>
+          <Form.Control
+            type="text"
+            id="emergency_names"
+            name="emergency_names"
+            placeholder="Enter Emergency Names"
+            onChange={handleChange}
+            value={tenantDetails.emergency_names}
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">Emergency Relation:</Form.Label>
+          <Form.Control
+            type="text"
+            id="emergency_relation"
+            name="emergency_relation"
+            placeholder="Enter Emergency Relation"
+            onChange={handleChange}
+            value={tenantDetails.emergency_relation}
+          />
+        </Form.Group>
+      </Col>
+      <Col sm="12" lg="6">
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="name">Emergency Phone Number:</Form.Label>
+          <Form.Control
+            type="text"
+            id="emergency_phone_number"
+            name="emergency_phone_number"
+            placeholder="Enter Emergency Phone Number"
+            onChange={handleChange}
+            value={tenantDetails.emergency_phone_number}
+          />
+        </Form.Group>
+      </Col>
+
+      <Row>
+        <Col sm="6" lg="6">
+          <Button type="submit" variant="btn btn-primary">
+            Submit
+          </Button>
+        </Col>
+        <Col sm="6" lg="6">
+          <Button type="button" variant="btn btn-danger">
+            cancel
+          </Button>
+        </Col>
+      </Row>{" "}
+    </Form>
+  </form>
+  )
+}
+
+export default UpdateTenants
